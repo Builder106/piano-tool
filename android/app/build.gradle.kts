6953fd4a2c0ff4753505345403701b03fad907e8
Builder106 <vaughanolayinka@gmail.com>
2026-09-12 20:21:02 -0400

    fix(android): set compileSdk to 37 for permission_handler

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.yinkavaughan.piano_tool"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
