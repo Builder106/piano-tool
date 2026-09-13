8d789b0106bc89f56941032c081bb8c7d8c4e72c
Olayinka Vaughan <114308865+Builder106@users.noreply.github.com>
2026-09-13 05:15:22 -0700

    fix(android): set compileSdk to 37 (#1)
    * fix(android): set compileSdk to 37 for permission_handler
    
    * fix(android): set compileSdkVersion to 37 for all subprojects
    
    * fix(android): safely configure subproject compileSdk checking executed state

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,29 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    val configureAndroid: () -> Unit = {
+        val android = project.extensions.findByName("android")
+        if (android != null) {
+            try {
+                val setCompileSdkVersion = android.javaClass.getMethod("setCompileSdkVersion", Int::class.javaPrimitiveType)
+                setCompileSdkVersion.invoke(android, 37)
+            } catch (_: NoSuchMethodException) {
+                try {
+                    val compileSdkProp = android.javaClass.getMethod("setCompileSdk", java.lang.Integer::class.java)
+                    compileSdkProp.invoke(android, 37)
+                } catch (_: Exception) {}
+            } catch (_: Exception) {}
+        }
+    }
+
+    if (project.state.executed) {
+        configureAndroid()
+    } else {
+        project.afterEvaluate { configureAndroid() }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
